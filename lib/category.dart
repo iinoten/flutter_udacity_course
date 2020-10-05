125d18998e7ebcc33d9fd2f83199cd2aaa33edcf
iinoten <nteten0313@icloud.com>
2020-10-06 01:30:26 +0900

    お手本通りに引数を取れるようにした

--- lib/category.dart
+++ lib/category.dart
@@ -6,17 +6,31 @@
 // https://www.dartlang.org/guides/language/effective-dart/style#ordering
 import 'package:flutter/material.dart';
 
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
 /// A custom [Category] widget.
 ///
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
 /// a colored [InkWell] animation.
 class Category extends StatelessWidget {
+  final String name;
+  final ColorSwatch color;
+  final IconData iconLocation;
   /// Creates a [Category].
   ///
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
   // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category();
+  const Category({
+    Key key,
+    @required this.name,
+    @required this.color,
+    @required this.iconLocation,
+  }): assert(name != null),
+      assert(color != null),
+      assert(iconLocation != null),
+      super(key: key);
 
   /// Builds a custom widget that shows [Category] information.
   ///
@@ -29,10 +43,11 @@ class Category extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Build the custom widget here, referring to the Specs.
     return Container(
+      height: _rowHeight,
       child: Padding(
         padding: EdgeInsets.all(8.0),
         child: InkWell(
-          borderRadius: BorderRadius.circular(50.0),  // InkWellに角丸を追加
+          borderRadius: _borderRadius,  // InkWellに角丸を追加
           onTap: () { print('Iwas tapped!');},
           splashColor: Colors.green[400],
           highlightColor: Colors.green[400],
@@ -41,7 +56,7 @@ class Category extends StatelessWidget {
               Padding(
                 padding: EdgeInsets.all(16.0),
                 child: Icon(
-                    Icons.cake, 
+                    iconLocation, 
                     size: 60.0, 
                   ),
               ),
