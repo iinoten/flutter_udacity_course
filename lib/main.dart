3f12d068310f83f779929194f6bd56a7964989dd
iinoten <nteten0313@icloud.com>
2020-10-05 20:27:59 +0900

    レッスン1, Quiz: Code Your Own Category Widget Inの項

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,39 @@
+
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
 
+// You can use a relative import, i.e. `import 'category.dart';` or
+// a package import, as shown below.
+// More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
+import 'package:hello_rectangle/category.dart';
+
+// TODO: Pass this information into your custom [Category] widget
+const _categoryName = 'Cake';
+const _categoryIcon = Icons.cake;
+const _categoryColor = Colors.green;
+
+/// The function that is called when main.dart is run.
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('HelloRectangle'),
-        ),
-        body: HelloRectangle(),
-      ),
-    ),
-  );
+  runApp(UnitConverterApp());
 }
 
-class HelloRectangle extends StatelessWidget {  // StatelessWidgetを継承
-
+/// This widget is the root of our application.
+/// Currently, we just show one widget in our app.
+class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(  //Centerウィジェットを親要素に追加して四角いウィジェットを中央に配置
-      child: Container( //子要素に四角いのを指定
-        color: Colors.blueAccent,
-        height: 400.0,
-        width: 300.0,
-        child: Center(
-          child: Text(
-            'Hello!',
-            style: TextStyle( fontSize: 40.0 ),
-          ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: Scaffold(
+        backgroundColor: Colors.green[100],
+        body: Center(
+          // TODO: Determine what properties you'll need to pass into the widget
+          child: Category(),
         ),
       ),
     );
